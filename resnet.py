3a386ac4c96cc72b56041622d35b7d58a084531f
SAFA CICEK <safacicek@gmail.com>
2018-07-23 10:36:29 -0700

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -14,6 +14,7 @@ import torch.nn.functional as F
 
 from torch.autograd import Variable
 
+is_bn = True
 
 def conv3x3(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
@@ -37,8 +38,12 @@ class BasicBlock(nn.Module):
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
+        if is_bn:
+            out = F.relu(self.bn1(self.conv1(x)))
+            out = self.bn2(self.conv2(out))
+        else:
+            out = F.relu(self.conv1(x))
+            out = self.conv2(out)            
         out += self.shortcut(x)
         out = F.relu(out)
         return out
@@ -50,7 +55,7 @@ class PreActBlock(nn.Module):
 
     def __init__(self, in_planes, planes, stride=1):
         super(PreActBlock, self).__init__()
-        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.bn1 = nn.BatchNorm2d(in_planes) 
         self.conv1 = conv3x3(in_planes, planes, stride)
         self.bn2 = nn.BatchNorm2d(planes)
         self.conv2 = conv3x3(planes, planes)
@@ -61,13 +66,18 @@ class PreActBlock(nn.Module):
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(x))
+        if is_bn:
+            out = F.relu(self.bn1(x))
+        else:
+            out = F.relu(x)
         shortcut = self.shortcut(out) if hasattr(self, 'shortcut') else x
         out = self.conv1(out)
-        out = self.conv2(F.relu(self.bn2(out)))
+        if is_bn:
+            out = self.conv2(F.relu(self.bn2(out)))
+        else:
+            out = self.conv2(F.relu(out))
         out += shortcut
         return out
-
 
 class Bottleneck(nn.Module):
     expansion = 4
@@ -89,44 +99,20 @@ class Bottleneck(nn.Module):
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
-        out = self.bn3(self.conv3(out))
+        if is_bn:
+            out = F.relu(self.bn1(self.conv1(x)))
+            out = F.relu(self.bn2(self.conv2(out)))
+            out = self.bn3(self.conv3(out))
+        else:
+            out = F.relu(self.conv1(x))
+            out = F.relu(self.conv2(out))
+            out = self.conv3(out)        
         out += self.shortcut(x)
         out = F.relu(out)
         return out
 
-
-class PreActBottleneck(nn.Module):
-    '''Pre-activation version of the original Bottleneck module.'''
-    expansion = 4
-
-    def __init__(self, in_planes, planes, stride=1):
-        super(PreActBottleneck, self).__init__()
-        self.bn1 = nn.BatchNorm2d(in_planes)
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
-
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(x))
-        shortcut = self.shortcut(out) if hasattr(self, 'shortcut') else x
-        out = self.conv1(out)
-        out = self.conv2(F.relu(self.bn2(out)))
-        out = self.conv3(F.relu(self.bn3(out)))
-        out += shortcut
-        return out
-
-
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=200):
+    def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
@@ -137,7 +123,7 @@ class ResNet(nn.Module):
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.linear = nn.Linear(512*block.expansion, num_classes)
-
+        
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
@@ -146,29 +132,30 @@ class ResNet(nn.Module):
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
-        return out
-
-
-def ResNet10():
-    return ResNet(PreActBlock, [1,1,1,1])
-
-def ResNet12():
-    return ResNet(PreActBlock, [1,1,1,2])
-
-def ResNet14():
-    return ResNet(PreActBlock, [2,2,1,1])
-
-def ResNet18():
-    return ResNet(PreActBlock, [2,2,2,2])
+    def forward(self, x, out_middle=False, from_middle=False):
+        if not from_middle:
+            if is_bn:
+                out = F.relu(self.bn1(self.conv1(x)))
+            else:
+                out = F.relu(self.conv1(x))
+            first_layer = self.layer1(out)
+    
+            out = self.layer2(first_layer)
+            out = self.layer3(out)
+            out = self.layer4(out)
+            out = F.avg_pool2d(out, 4)
+            feat = out.view(out.size(0), -1)
+        else:
+            feat = x
+        
+        out = self.linear(feat)
+        if out_middle:
+            return out, feat
+        else:
+            return out
+            
+def ResNet18(n=10):
+    return ResNet(PreActBlock, [2,2,2,2], n)
 
 def ResNet34():
     return ResNet(BasicBlock, [3,4,6,3])
@@ -183,9 +170,3 @@ def ResNet152():
     return ResNet(Bottleneck, [3,8,36,3])
 
 
-def test():
-    net = ResNet18()
-    y = net(Variable(torch.randn(1,3,32,32)))
-    print(y.size())
-
-# test()
